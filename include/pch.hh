ed4bfe52334e2cc5288ef641649a6814f5e4f2ba
Segfault (Core dumped) <tyiwei0@gmail.com>
2024-12-01 05:06:21 +0800

    Misc: Converted common headers to pch

---
+++ include/pch.hh
@@ -0,0 +1,17 @@
+#if !defined(PCH_HH)
+#define PCH_HH
+
+// Stdlib
+#include <cstdint>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+// SDL
+#include "SDL.h"
+#include "SDL_image.h"
+#include "SDL_ttf.h"
+
+#endif
